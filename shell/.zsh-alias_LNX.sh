a8e89312ad4269dc1aad614823a2d456d93658f0
misigno <efournier92@gmail.com>
2018-05-20 17:40:04 -0400

    update shell/.zsh-alias

--- shell/.zsh-alias_LNX.sh
+++ shell/.zsh-alias_LNX.sh
@@ -2,70 +2,44 @@ alias icecream='/etc/openvpn;sudo openvpn --daemon --config Icecream.ovpn'
 alias icecream!='sudo killall openvpn'
 alias tor='torbrowser-launcher'
 
-alias drive_up='sudo rclone sync /mnt/BNK/Sync Drive: --delete-during --drive-skip-gdocs --verbose'
-alias drive_dwn='sudo rclone sync Drive: /mnt/BNK/Sync --delete-during --drive-skip-gdocs --verbose'
+alias bnkMnt='veracrypt /dev/disk/by-partuuid/9bd48f56-7489-487c-9c96-9e145b637426 /mnt/BNK --slot=33'
+alias bnkMnt!='veracrypt -t -d /dev/disk/by-partuuid/9bd48f56-7489-487c-9c96-9e145b637426'
 
-alias extA='veracrypt --mount /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part2 --slot=8'
-alias extA!='veracrypt -t -d /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part2'
+alias extA='veracrypt --mount /dev/disk/by-partuuid/7684c170-a88e-4db8-8759-5a80e99ad74b /mnt/extA --slot=8'
+alias extA!='veracrypt -t -d /dev/disk/by-partuuid/7684c170-a88e-4db8-8759-5a80e99ad74b'
 
-alias bnkA='veracrypt --mount /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part3 --slot=9'
-alias bnkA!='veracrypt -t -d /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part3'
-
-alias lnxA='veracrypt --mount /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part6 --slot=10'
-alias lnxA!='veracrypt -t -d /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part6'
-
-alias w10A='veracrypt --mount /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part7 --slot=11'
-alias w10A!='veracrypt -t -d /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part7'
-
-alias extB='veracrypt --mount /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part2 --slot=12'
+alias extB='veracrypt --mount /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part2 /mnt/extB --slot=12'
 alias extB!='veracrypt -t -d /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part2'
 
-alias bnkB='veracrypt --mount /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part3 --slot=13'
-alias bnkB!='veracrypt -t -d /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part3'
-
-alias lnxB='veracrypt --mount /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part6 --slot=14'
-alias lnxB!='veracrypt -t -d /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part6'
-
-alias w10B='veracrypt --mount /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part7 --slot=15'
-alias w10B!='veracrypt -t -d /dev/disk/by-id/usb-JMicron_JMicron_DB987654321CC-0:0-part7'
-
-alias extC='veracrypt --mount /dev/disk/by-id/wwn-0x5002538900000000-part2 --slot=16'
+alias extC='veracrypt --mount /dev/disk/by-id/wwn-0x5002538900000000-part2 /mnt/extC --slot=16'
 alias extC!='veracrypt -t -d /dev/disk/by-id/wwn-0x5002538900000000-part2'
 
-alias bnkC='veracrypt --mount /dev/disk/by-id/wwn-0x5002538900000000-part3 --slot=17'
-alias bnkC!='veracrypt -t -d /dev/disk/by-id/wwn-0x5002538900000000-part3'
+alias MD='veracrypt --mount /dev/disk/by-partuuid/2824c02b-422c-4702-acd7-d8bbe8bdcbbf /mnt/MD --slot=20'
+alias MD!='veracrypt -t -d /dev/disk/by-partuuid/2824c02b-422c-4702-acd7-d8bbe8bdcbbf'
 
-alias lnxC='veracrypt --mount /dev/disk/by-id/wwn-0x5002538900000000-part5 --slot=18'
-alias lnxC!='veracrypt -t -d /dev/disk/by-id/wwn-0x5002538900000000-part5'
+alias gryEx='veracrypt --mount /dev/disk/by-partuuid/cf03b736-d2cd-44dd-9578-d68844421b83 /mnt/gryEx --slot=21'
+alias gryEx!='veracrypt -t -d /dev/disk/by-partuuid/cf03b736-d2cd-44dd-9578-d68844421b83'
 
-alias winC='veracrypt --mount /dev/disk/by-id/wwn-0x5002538900000000-part6 --slot=19'
-alias winC!='veracrypt -t -d /dev/disk/by-id/wwn-0x5002538900000000-part6'
+alias gryNt='veracrypt --mount /dev/disk/by-partuuid/b56d5683-3653-4b07-8054-89ea93d058e6 /mnt/gryNt --slot=22'
+alias gryNt!='veracrypt -t -d /dev/disk/by-partuuid/b56d5683-3653-4b07-8054-89ea93d058e6'
 
-alias MD='veracrypt --mount /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part2 --slot=20'
-alias MD!='veracrypt -t -d /dev/disk/by-id/usb-SABRENT_SABRENT_DB98765432143-0:0-part2'
-
-alias gryEx='veracrypt --mount /dev/disk/by-id/usb-Seagate_BUP_Slim_SL_NA794A75-0:0-part2 --slot=21'
-alias gryEx!='veracrypt -t -d /dev/disk/by-id/usb-Seagate_BUP_Slim_SL_NA794A75-0:0-part2'
-
-alias gryNt='veracrypt --mount /dev/disk/by-id/usb-Seagate_BUP_Slim_SL_NA794A75-0:0-part3 --slot=22'
-alias gryNt!='veracrypt -t -d /dev/disk/by-id/usb-Seagate_BUP_Slim_SL_NA794A75-0:0-part3'
-
-alias blkEx='veracrypt --mount /dev/disk/by-id/usb-Seagate_Backup+_BK_NA75NCP3-0:0-part2 --slot=23'
+alias blkEx='veracrypt --mount /dev/disk/by-id/usb-Seagate_Backup+_BK_NA75NCP3-0:0-part2 /mnt/blkEx --slot=23'
 alias blkEx!='veracrypt -t -d /dev/disk/by-id/usb-Seagate_Backup+_BK_NA75NCP3-0:0-part2'
 
-alias bk_extAB='sudo rclone sync /media/veracrypt8 /media/veracrypt12 --verbose'
-alias bk_extAC='sudo rclone sync /media/veracrypt8 /media/veracrypt16 --verbose'
-alias bk_extBA='sudo rclone sync /media/veracrypt12 /media/veracrypt8 --verbose'
-alias bk_extBC='sudo rclone sync /media/veracrypt12 /media/veracrypt16 --verbose'
-alias bk_extCA='sudo rclone sync /media/veracrypt16 /media/veracrypt8 --verbose'
-alias bk_extCB='sudo rclone sync /media/veracrypt16 /media/veracrypt12 --verbose'
+alias bk_extAB='sudo rclone sync /mnt/extA /mnt/extB --verbose'
+alias bk_extAC='sudo rclone sync /mnt/extA /mnt/extC --verbose'
+alias bk_extBA='sudo rclone sync /mnt/extB /mnt/extA --verbose'
+alias bk_extBC='sudo rclone sync /mnt/extB /mnt/extC --verbose'
+alias bk_extCA='sudo rclone sync /mnt/extC /mnt/extA --verbose'
+alias bk_extCB='sudo rclone sync /mnt/extC /mnt/extB --verbose'
+alias bk_AMD='sudo rclone sync /mnt/extA/Media /mnt/MD --verbose'
 
-alias bk_bnkA='sudo rclone sync /mnt/BNK /media/veracrypt9 --verbose'
-alias bk_bnkB='sudo rclone sync /mnt/BNK /media/veracrypt13 --verbose'
-alias bk_bnkC='sudo rclone sync /mnt/BNK /media/veracrypt17 --verbose'
+alias bk_bnkA='sudo rclone sync /mnt/BNK /mnt/BKA/BNK --verbose'
+alias bk_bnkB='sudo rclone sync /mnt/BNK /mnt/BKB/BNK --verbose'
+alias bk_bnkC='sudo rclone sync /mnt/BNK /mnt/BKC/BNK --verbose'
 
-alias bk_lnxA='sudo dd if=/dev/sda8 of=/media/veracrypt10/XX-XX-XX.img status=progress'
-alias bk_lnxB='sudo dd if=/dev/sda8 of=/media/veracrypt14/XX-XX-XX.img status=progress'
-alias bk_lnxC='sudo dd if=/dev/sda8 of=/media/veracrypt18/XX-XX-XX.img status=progress'
+alias driveUp='sudo rclone sync /mnt/BNK/Sync Drive: --delete-during --drive-skip-gdocs --verbose'
+alias driveDwn='sudo rclone sync Drive: /mnt/BNK/Sync --delete-during --drive-skip-gdocs --verbose'
 
 alias fournierDesktop_chrome='google-chrome --profile-directory="Profile 1" --app-id=gbchcmhmhahfdphkhkmpfmihenigjmpp'
+
