484254769dc7f97be07425ccaa6ea1af7ab199b1
misigno <efournier92@gmail.com>
2018-07-12 18:33:00 -0400

    enhance .AF.sh

--- Ubuntu/.AF.sh
+++ Ubuntu/.AF.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-google-chrome --new-window "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=whole+foods+salad" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=cashews" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=vegetable+rotini" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kerrygold+cheese" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=cabot+shredded" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=lettuce" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bell+pepper" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=croutons" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bacon+bits" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=pineapple+chunks" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=watermelon" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=blackberries" \
--d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kraft+dressing" \
--d "https://www.amazon.com/AmazonFresh/"
+SEARCH_BASE="https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords="
+HOMEPAGE="https://www.amazon.com/AmazonFresh/"
+
+google-chrome --new-window $HOMEPAGE -d "${SEARCH_BASE}cashews"
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=vegetable+rotini" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kerrygold+cheese" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=cabot+shredded" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=lettuce" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bell+pepper" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=croutons" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bacon+bits" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=pineapple+chunks" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=watermelon" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=blackberries" \
+# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kraft+dressing" \
+# -d "https://www.amazon.com/AmazonFresh/"
 
