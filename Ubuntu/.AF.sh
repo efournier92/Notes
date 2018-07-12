3c5caca8a7b30caf12afd4e27677b7a89da10da8
misigno <efournier92@gmail.com>
2018-07-12 18:38:34 -0400

    reorder .AF.sh

--- Ubuntu/.AF.sh
+++ Ubuntu/.AF.sh
@@ -4,18 +4,18 @@ SEARCH="https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh
 HOMEPAGE="https://www.amazon.com/AmazonFresh/"
 
 google-chrome --new-window $HOMEPAGE \
--d "${SEARCH}cashews" \
 -d "${SEARCH}whole+foods+salad" \
--d "${SEARCH}vegetable+rotini" \
--d "${SEARCH}kerrygold+cheese" \
--d "${SEARCH}cabot+shredded" \
 -d "${SEARCH}lettuce" \
 -d "${SEARCH}bell+pepper" \
 -d "${SEARCH}croutons" \
 -d "${SEARCH}bacon+bits" \
+-d "${SEARCH}kraft+dressing" \
 -d "${SEARCH}pineapple+chunks" \
 -d "${SEARCH}watermelon" \
 -d "${SEARCH}blackberries" \
--d "${SEARCH}kraft+dressing" \
+-d "${SEARCH}kerrygold+cheese" \
+-d "${SEARCH}cabot+shredded" \
+-d "${SEARCH}cashews" \
+-d "${SEARCH}vegetable+rotini" \
 -d $HOMEPAGE
 
