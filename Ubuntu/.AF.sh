bb1ad301710cb0633e3a3c70106cb19f9751a731
misigno <efournier92@gmail.com>
2018-07-12 18:16:11 -0400

    add .AF.sh

---
+++ Ubuntu/.AF.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+google-chrome --new-window "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=whole+foods+salad" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=cashews" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=vegetable+rotini" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kerrygold+cheese" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=cabot+shredded" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=lettuce" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bell+pepper" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=croutons" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bacon+bits" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=pineapple+chunks" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=watermelon" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=blackberries" \
+-d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kraft+dressing" \
+-d "https://www.amazon.com/AmazonFresh/"
+
