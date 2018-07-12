96dd8e55a8adef570170a57362bdb0480b763580
misigno <efournier92@gmail.com>
2018-07-12 18:34:57 -0400

    finish adding .AF.sh variables

--- Ubuntu/.AF.sh
+++ Ubuntu/.AF.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-SEARCH_BASE="https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords="
+SEARCH="https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords="
 HOMEPAGE="https://www.amazon.com/AmazonFresh/"
 
-google-chrome --new-window $HOMEPAGE -d "${SEARCH_BASE}cashews"
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=vegetable+rotini" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kerrygold+cheese" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=cabot+shredded" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=lettuce" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bell+pepper" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=croutons" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=bacon+bits" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=pineapple+chunks" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=watermelon" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=blackberries" \
-# -d "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Damazonfresh&field-keywords=kraft+dressing" \
-# -d "https://www.amazon.com/AmazonFresh/"
+google-chrome --new-window $HOMEPAGE \
+-d "${SEARCH}cashews" \
+-d "${SEARCH}whole+foods+salad" \
+-d "${SEARCH}vegetable+rotini" \
+-d "${SEARCH}kerrygold+cheese" \
+-d "${SEARCH}cabot+shredded" \
+-d "${SEARCH}lettuce" \
+-d "${SEARCH}bell+pepper" \
+-d "${SEARCH}croutons" \
+-d "${SEARCH}bacon+bits" \
+-d "${SEARCH}pineapple+chunks" \
+-d "${SEARCH}watermelon" \
+-d "${SEARCH}blackberries" \
+-d "${SEARCH}kraft+dressing" \
+-d $HOMEPAGE
 
