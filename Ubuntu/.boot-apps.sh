b0e86e3c2b7d3ee67dbfc66e212f06ace2db0ec4
misigno <efournier92@gmail.com>
2018-07-25 19:32:47 -0400

    edit .boot-apps for 1 password prompt

--- Ubuntu/.boot-apps.sh
+++ Ubuntu/.boot-apps.sh
@@ -5,21 +5,21 @@ BOARD_ID=$(dmidecode -s baseboard-serial-number)
 MB2015_ID='C02639600FUGQ101K'
 MHUB_ID='C0715060AXMDKP2AQ'
 
-sudo -u misigno copyq &
+sudo -u $USER copyq &
 git config --global credential.helper "cache --timeout=1512000"
 
 if [ $BOARD_ID = $MB2015_ID ]; then
   sleep 1
-  -u misigno redshift -l 40.7:-73.9851 -t 5700:3800 -g 0.8 -m randr &
+  redshift -l 40.7:-73.9851 -t 5700:3800 -g 0.8 -m randr &
   systemctl start mongodb &
-  sudo -u misigno veracrypt /dev/disk/by-partuuid/e68c8ec3-9c49-489c-9a0f-339f866920d6 /mnt/BNK --slot=33 &
+  sudo -u $USER veracrypt /dev/disk/by-partuuid/e68c8ec3-9c49-489c-9a0f-339f866920d6 /mnt/BNK --slot=33 &
 
 elif [ $BOARD_ID = $MHUB_ID ]; then
   sleep 1
-  sudo -u misigno veracrypt --mount /dev/disk/by-partuuid/2824c02b-422c-4702-acd7-d8bbe8bdcbbf /mnt/MD --slot=20 &
-  sudo -u misigno kodi -fs &
+  sudo -u $USER veracrypt --mount /dev/disk/by-partuuid/2824c02b-422c-4702-acd7-d8bbe8bdcbbf /mnt/MD --slot=20 &
+  sudo -u $USER kodi -fs &
 
 else
-  nohup redshift -l 40.7:-73.9851 -t 5700:3800 -g 0.8 -m randr &
+  redshift -l 40.7:-73.9851 -t 5700:3800 -g 0.8 -m randr &
 fi
 
