9f9efc5cf9b12eb9f5fa25dcc4896362d299f2b4
misigno <efournier92@gmail.com>
2018-07-30 08:23:18 -0400

    update env-totres to launch mongod

--- Ubuntu/.env-totes.sh
+++ Ubuntu/.env-totes.sh
@@ -5,7 +5,7 @@ tmux new-session -d -s totesnote
 tmux switch -t totesnote
 
 tmux new-window -t totesnote:1
-tmux send-keys -t totesnote:1 "cd /mnt/BNK/CodeSpace/totesnote;vim" C-m
+tmux send-keys -t totesnote:1 "pkexec /usr/bin/mongod --quiet --config /etc/mongod.conf & cd /mnt/BNK/CodeSpace/totesnote;vim ." C-m
 
 tmux new-window -t totesnote:2
 tmux select-window -t totesnote:2
@@ -16,15 +16,12 @@ tmux send-keys -t totesnote:2 "cd /mnt/BNK/CodeSpace/totesnote;ng serve" C-m
 
 tmux new-window -t totesnote:3
 tmux select-window -t totesnote:3
-tmux send-keys -t totesnote:3 "cd /mnt/BNK/CodeSpace/totesnote-api;vim" C-m
+tmux send-keys -t totesnote:3 "cd /mnt/BNK/CodeSpace/totesnote-api;vim ." C-m
 
 tmux new-window -t totesnote:4
 tmux select-window -t totesnote:4
 tmux send-keys -t totesnote:4 "cd /mnt/BNK/CodeSpace/totesnote-api" C-m
 tmux split-window -h -t totesnote:4
 tmux select-pane -t 2
-tmux send-keys -t totesnote:4 "cd /mnt/BNK/CodeSpace/totesnote-api;mongod" C-m
-tmux split-window -t totesnote:4
-tmux select-pane -t 3
 tmux send-keys -t totesnote:4 "cd /mnt/BNK/CodeSpace/totesnote-api;nodemon app.js" C-m
 
