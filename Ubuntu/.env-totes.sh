abb9bfbcc6b29da8184a47786f797986d15b2195
misigno <efournier92@gmail.com>
2018-08-08 19:36:01 -0400

    finish .env-totes.sh

--- Ubuntu/.env-totes.sh
+++ Ubuntu/.env-totes.sh
@@ -4,8 +4,7 @@ tmux new-session -d -s totesnote
 
 tmux switch -t totesnote
 
-tmux new-window -t totesnote:1
-tmux send-keys -t totesnote:1 "pkexec /usr/bin/mongod --quiet --config /etc/mongod.conf & cd /mnt/BNK/CodeSpace/totesnote"
+tmux send-keys -t totesnote:1 "pkexec /usr/bin/mongod --quiet --config /etc/mongod.conf & cd /mnt/BNK/CodeSpace/totesnote" C-m
 tmux send-keys -t totesnote:1 "vim ." C-m
 
 tmux new-window -t totesnote:2
@@ -17,7 +16,8 @@ tmux send-keys -t totesnote:2 "cd /mnt/BNK/CodeSpace/totesnote;ng serve" C-m
 
 tmux new-window -t totesnote:3
 tmux select-window -t totesnote:3
-tmux send-keys -t totesnote:3 "cd /mnt/BNK/CodeSpace/totesnote-api;vim ." C-m
+tmux send-keys -t totesnote:3 "cd /mnt/BNK/CodeSpace/totesnote-api" C-m
+tmux send-keys -t totesnote:3 "vim ." C-m
 
 tmux new-window -t totesnote:4
 tmux select-window -t totesnote:4
