6c331a54b3ffd5605c7bc525b98a12f242fc7642
misigno <efournier92@gmail.com>
2019-12-30 14:07:48 -0500

    further refactor aud-log.bash

--- Scripts/Bash/aud-log.bash
+++ Scripts/Bash/aud-log.bash
@@ -6,8 +6,7 @@
 
 # ^ BEST READ FROM THE BOTTOM UP ^
 
-is_convert_complete=false
-
+# Get option parameters
 while getopts "vi:o:" OPTION
 do
   case $OPTION in
@@ -24,25 +23,37 @@ do
   esac
 done
 
-# Stop execution if either directory is invalid
-if [[ ! -d "$in_dir" || -z "$out_dir" ]]; then
-  echo "Input and output directories expected (use -i and -o)"
-  exit
-else
-  if [[ $verbose = true ]]; then
-    echo "INPUT DIRECTORY: $in_dir"
-    echo "OUTPUT DIRECTORY: $out_dir"
-  fi
-fi
+# Return CONCAT DIR location
+function concat_dir {
+  echo "$out_dir/-concat"
+}
+
+# Return JOIN DIR location
+function join_dir {
+  echo "$out_dir/-join"
+}
+
+# Return CONCAT FILE location, based on input date
+function concat_file {
+  date=`parse_date $1`
+  dir=`concat_dir`
+  echo "$dir/$date.txt"
+}
+
+# Return MP3 FILE location
+function mp3_file {
+  date="$1"
+  echo "$out_dir/$date.mp3"
+}
 
 # True if .WAV file is valid
-is_valid_file() {
+function is_valid_file {
   eval_file=$1
   [[ "$eval_file" = *.WAV ]] && [[ ! "$eval_file" =~ [@#\$%^\&*+] ]]
 }
 
 # True if input is of valid date format
-is_valid_file_name() {
+function is_valid_file_name {
   eval_file=$1
   file_name="${eval_file##*/}"
   file_name="${file_name%%.*}"
@@ -55,55 +66,8 @@ is_valid_file_name() {
   fi
 }
 
-# Play input file in VLC
-play_in_vlc() {
-  eval_file="$1"
-  [[ $verbose = true ]] && echo "Opening $eval_file in VLC"
-  vlc "$eval_file"
-}
-
-# Join files from the same date
-join_duplicates() {
-  join_dir=`join_dir`
-  mkdir -p "$join_dir"
-  in_dir="$join_dir"
-  [[ $verbose = true ]] && echo "Joining duplicates into `join_dir`"
-  name_audio_files
-  convert_audio_files
-}
-
-function join_dir_has_wavs {
-  if ls `join_dir`/*.WAV &>/dev/null &>/dev/null
-  then
-    true
-  else
-    false
-  fi
-}
-
-# Wait for FFmpeg to finish converting before joining files from the same date
-wait_for_ffmpeg_to_finish() {
-  dir=$1
-  [[ $verbose = true ]] && echo "Started waiting for FFmpeg to finish"
-  time_spent_waiting=0
-  until [[ $is_convert_complete == true || $time_spent_waiting -gt 1000 ]]; do
-    sleep 2
-    [[ $verbose = true ]] && echo "Spent $time_spent_waiting waiting ofr FFmpeg"
-    time_spent_waiting=$((time_spent_waiting + 5))
-    ffmpeg_windows=`tmux list-windows | grep -i 'ffmpeg'`
-    if [[ $ffmpeg_windows == '' ]]; then
-      [[ $verbose = true ]] && echo "FFmpeg conversion complete"
-      is_convert_complete=true
-      if join_dir_has_wavs; then
-        in_dir=`join_dir`
-        start_conversion
-      fi
-    fi
-  done
-}
-
 # True if input is of valid date format
-parse_date() {
+function parse_date {
   eval_file=$1
   file_name="${eval_file##*/}"
   file_name="${file_name%%.*}"
@@ -111,14 +75,30 @@ parse_date() {
   echo "$file_name"
 }
 
-rename_file() {
-  mp3_file=$1
-  [[ $verbose = true ]] && echo "Moving $mp3_file from $in_dir to $join_dir/${date}_00.WAV for joining"
-  mv "$mp3_file" "$join_dir/${date}_00.WAV"
-  mp3_file="$join_dir/${date}_01.WAV"
+# Wait for FFmpeg to finish converting before joining files from the same date
+function wait_for_ffmpeg_to_finish {
+  [[ $verbose = true ]] && echo "Started waiting for FFmpeg to finish"
+  time_spent_waiting=0
+  # Loop until FFmpeg conversion is finished
+  until [[ $is_convert_complete == true || $time_spent_waiting -gt 10000 ]]; do
+    seconds_to_wait=1
+    sleep $seconds_to_wait
+    [[ $verbose = true ]] && echo "Waiting for FFmpeg: $time_spent_waiting seconds"
+    time_spent_waiting=$((time_spent_waiting + $seconds_to_wait))
+    ffmpeg_windows=`tmux list-windows | grep -i 'ffmpeg'`
+    if [[ $ffmpeg_windows == '' ]]; then
+      [[ $verbose = true ]] && echo "FFmpeg conversion complete"
+      is_convert_complete=true
+      # Join duplicates, if there are any
+      if ls `join_dir`/*.WAV &>/dev/null &>/dev/null; then
+        in_dir=`join_dir`
+        start_conversion
+      fi
+    fi
+  done
 }
 
-convert_audio_files() {
+function convert_audio_files {
   for file_to_convert in $in_dir/*; do
     [[ $file_to_convert = *.WAV ]] || continue
     if ! is_valid_file "$file_to_convert"; then
@@ -142,6 +122,8 @@ convert_audio_files() {
     if [[ "$file_to_convert" =~ "_00" ]]; then
       [[ $verbose = true ]] && echo "First File: $file_to_convert"
       if test -f "$mp3_file"; then
+        mkdir -p `join_dir`
+        # TODO: Add check
         mv "$mp3_file" "`join_dir`/${date}_AA.WAV"
         mp3_file="`join_dir`/${date}_BB.WAV"
       fi
@@ -157,14 +139,11 @@ convert_audio_files() {
 
       tmux new-window "ffmpeg -f concat -safe 0 -i $concat_file -c:a libmp3lame -ac 1 -q:a 15 $mp3_file"
     fi
-
   done
   wait_for_ffmpeg_to_finish
-  # if [[ $must_join_files == true ]]; then
-  # fi
 }
 
-clean_concat_file() {
+function clean_concat_file {
   clean_date=`parse_date "$1"`
   file_to_clean=`concat_file "$clean_date"`
   mkdir -p `concat_dir`
@@ -173,29 +152,8 @@ clean_concat_file() {
   touch "$file_to_clean"
 }
 
-concat_dir() {
-  echo "$out_dir/.concat"
-}
-
-join_dir() {
-  join_dir="$out_dir/.join"
-  mkdir -p "$join_dir"
-  echo "$join_dir"
-}
-
-concat_file() {
-  date=`parse_date $1`
-  dir=`concat_dir`
-  echo "$dir/$date.txt"
-}
-
-mp3_file() {
-  date="$1"
-  echo "$out_dir/$date.mp3"
-}
-
 # Rename files based on user input
-name_audio_files() {
+function name_audio_files {
   for file in $in_dir/*.WAV; do
     if ! is_valid_file "$file"; then
       continue
@@ -203,9 +161,12 @@ name_audio_files() {
     new_file_name="$file"
     has_file_moved=false
     until is_valid_file_name "$new_file_name"; do
-      play_in_vlc "$file"
+      cvlc "$file" &
+      vlc_process=$!
+      sleep 0.1
       printf "[yy-mm-dd_##]: "
       read new_file_name
+      kill $vlc_process
       if [[ "$new_file_name" == "delete" ]]; then
         rm "$file"
         break
@@ -224,10 +185,11 @@ name_audio_files() {
 }
 
 function ask_to_delete_join_dir {
-  [[ ! -d `join_dir` ]] && return 0
+  [ ! -d `join_dir` ] && return 0
+  echo "-------------"
   echo "JOIN CONTENTS"
   echo "-------------"
-  ls `join_dir`
+  ls -1 `join_dir`
   until [[ $should_delete_join_dir == "y" || $should_delete_join_dir == "n" ]]; do
   echo "Do you want to delete the join directory? [y/n]: "
   read should_delete_join_dir
@@ -239,11 +201,35 @@ function ask_to_delete_join_dir {
   done
 }
 
+function list_new_mp3_files {
+  # TODO: Show lengths
+}
+
+function prompt_to_delete_inputs {
+
+}
+
+# Stop execution if either directory is invalid
+function ensure_valid_directories {
+if [[ ! -d "$in_dir" || -z "$out_dir" ]]; then
+  echo "Input and output directories expected (use -i and -o)"
+  exit
+else
+  if [[ $verbose = true ]]; then
+    echo "INPUT DIRECTORY: $in_dir"
+    echo "OUTPUT DIRECTORY: $out_dir"
+  fi
+fi
+}
+
 # Kickstart audio conversion process
-start_conversion() {
+function start_conversion {
+  ensure_valid_directories
   name_audio_files
   convert_audio_files
 }
 
+in_file_type="WAV"
+ask_to_delete_join_dir
 start_conversion
 
