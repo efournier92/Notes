fa761bbd01fb3225c2bfc93396b6ed0ea30fd353
misigno <efournier92@gmail.com>
2019-12-29 20:04:51 -0500

    start consolidating utlity functions in aud-log.bash

--- Scripts/Bash/aud-log.bash
+++ Scripts/Bash/aud-log.bash
@@ -10,18 +10,18 @@ is_convert_complete=false
 
 while getopts "vi:o:" OPTION
 do
-	case $OPTION in
-		v)
+  case $OPTION in
+    v)
       verbose=true
-			echo "VERBOSE OUTPUT ENABLED"
-			;;
-		i)
-			in_dir=$OPTARG
-			;;
-		o)
+      echo "VERBOSE OUTPUT ENABLED"
+      ;;
+    i)
+      in_dir=$OPTARG
+      ;;
+    o)
       out_dir=$OPTARG
-			;;
-	esac
+      ;;
+  esac
 done
 
 # Stop execution if either directory is invalid
@@ -64,10 +64,21 @@ play_in_vlc() {
 
 # Join files from the same date
 join_duplicates() {
-  mkdir -p `join_dir`
+  join_dir=`join_dir`
+  mkdir -p "$join_dir"
+  in_dir="$join_dir"
   [[ $verbose = true ]] && echo "Joining duplicates into `join_dir`"
-  name_audio_files `join_dir` true
-  convert_audio_files `join_dir`
+  name_audio_files
+  convert_audio_files
+}
+
+function join_dir_has_wavs {
+  if ls `join_dir`/*.WAV &>/dev/null &>/dev/null
+  then
+    true
+  else
+    false
+  fi
 }
 
 # Wait for FFmpeg to finish converting before joining files from the same date
@@ -76,14 +87,17 @@ wait_for_ffmpeg_to_finish() {
   [[ $verbose = true ]] && echo "Started waiting for FFmpeg to finish"
   time_spent_waiting=0
   until [[ $is_convert_complete == true || $time_spent_waiting -gt 1000 ]]; do
-    sleep 5
+    sleep 2
     [[ $verbose = true ]] && echo "Spent $time_spent_waiting waiting ofr FFmpeg"
     time_spent_waiting=$((time_spent_waiting + 5))
     ffmpeg_windows=`tmux list-windows | grep -i 'ffmpeg'`
     if [[ $ffmpeg_windows == '' ]]; then
       [[ $verbose = true ]] && echo "FFmpeg conversion complete"
       is_convert_complete=true
-      join_duplicates
+      if join_dir_has_wavs; then
+        in_dir=`join_dir`
+        start_conversion
+      fi
     fi
   done
 }
@@ -99,62 +113,55 @@ parse_date() {
 
 rename_file() {
   mp3_file=$1
-  [[ $verbose = true ]] && echo "Moving $mp3_file from $source_dir to $join_dir/${date}_00.WAV for joining"
+  [[ $verbose = true ]] && echo "Moving $mp3_file from $in_dir to $join_dir/${date}_00.WAV for joining"
   mv "$mp3_file" "$join_dir/${date}_00.WAV"
   mp3_file="$join_dir/${date}_01.WAV"
 }
 
-# Open a new TMUX window and run FFmpeg convert command
-convert_file_with_ffmpeg() {
-  convert_file="$1"
-  echo "CONVERT: $convert_file"
-  date=`parse_date "$convert_file"`
-  mp3_file=`mp3_file $date`
-  concat_file=`concat_file $date`
-  source_dir="$in_dir"
-
-  if [[ $verbose = true ]]; then
-    echo "CONVERTING WITH FFMPEG"
-    echo "Convert File: $convert_file"
-    echo "Date: $date"
-    echo "MP3 File: $mp3_file"
-    echo "Concat File: $concat_file"
-    echo "Source Directory: $source_dir"
-  fi
-
-  # if [[ "$convert_file" =~ "_00" ]]; then
-    [[ $verbose = true ]] && echo "First File: $convert_file"
-    mp3_file=`mp3_file $date`
-    if test -f "$mp3_file"; then
-      join_dir=`join_dir`
-      source_dir="$join_dir"
-      must_join_files=true
-      rename_file "$mp3_file"
-    fi
-    if [[ "$must_join_files" == true ]]; then
-      join_dir=`join_dir`
-      source_dir="$join_dir"
-    fi
-    write_concat_file "$date" "$source_dir"
-    tmux new-window "ffmpeg -f concat -safe 0 -i $concat_file -c:a libmp3lame -ac 1 -q:a 15 $mp3_file"
-  # fi
-}
-
-# 
 convert_audio_files() {
-  must_join_files=false
-  source_dir="$1"
-  for convert_file in $source_dir/*; do
-    [[ $convert_file = *.WAV ]] || continue
-    if ! is_valid_file "$convert_file"; then
-      [[ $verbose = true ]] && echo "Skipping invalid file $convert_file"
+  for file_to_convert in $in_dir/*; do
+    [[ $file_to_convert = *.WAV ]] || continue
+    if ! is_valid_file "$file_to_convert"; then
+      [[ $verbose = true ]] && echo "Skipping invalid file $file_to_convert"
       continue
     fi
-    convert_file_with_ffmpeg "$convert_file"
+
+    echo "CONVERT: $file_to_convert"
+    date=`parse_date "$file_to_convert"`
+    mp3_file=`mp3_file $date`
+    concat_file=`concat_file $date`
+
+    if [[ $verbose = true ]]; then
+      echo "CONVERTING WITH FFMPEG"
+      echo "Convert File: $file_to_convert"
+      echo "Date: $date"
+      echo "MP3 File: $mp3_file"
+      echo "Concat File: $concat_file"
+    fi
+
+    if [[ "$file_to_convert" =~ "_00" ]]; then
+      [[ $verbose = true ]] && echo "First File: $file_to_convert"
+      if test -f "$mp3_file"; then
+        mv "$mp3_file" "`join_dir`/${date}_AA.WAV"
+        mp3_file="`join_dir`/${date}_BB.WAV"
+      fi
+
+      # Write more than enough filenames to FFmpeg concat file
+      file_to_write=`concat_file "$date"`
+      clean_concat_file "$file_to_write"
+      [[ $verbose = true ]] && echo "Writing concat file $concat_file to $in_dir"
+      loops=9
+      for (( n = 0; n < $loops; n++ )); do
+        echo "file '$in_dir/${date}_0${n}.WAV'" >> "$file_to_write"
+      done
+
+      tmux new-window "ffmpeg -f concat -safe 0 -i $concat_file -c:a libmp3lame -ac 1 -q:a 15 $mp3_file"
+    fi
+
   done
-  if [[ $must_join_files == true ]]; then
-    wait_for_ffmpeg_to_finish
-  fi
+  wait_for_ffmpeg_to_finish
+  # if [[ $must_join_files == true ]]; then
+  # fi
 }
 
 clean_concat_file() {
@@ -187,74 +194,55 @@ mp3_file() {
   echo "$out_dir/$date.mp3"
 }
 
-# Write more than enough filenames to FFmpeg concat file
-write_concat_file() {
-  concat_date="$1"
-  concat_file_dir="$2"
-  file_to_write=`concat_file "$date"`
-  clean_concat_file "$file_to_write"
-  [[ $verbose = true ]] && echo "Writing concat file $concat_file to $concat_file_dir"
-  loops=9
-  if [[ "$must_join_files" == true ]]; then
-    loops=2
-  fi
-  for (( n=0; n < $loops; n++ )); do
-    echo "file '$concat_file_dir/${concat_date}_0${n}.WAV'" >> "$file_to_write"
-  done
-}
-
-# Collect user input for filename
-get_filename_input() {
-  in_file="$1"
-  dir="$2"
-  if ! is_valid_file "$in_file"; then
-    return
-  fi
-  new_file_name="$in_file"
-  has_file_moved=false
-  i=0
-  until [[ "$has_file_moved" == true ]] || [[ $i -gt 10 ]]; do
-    if ! is_valid_file_name "$new_file_name" || [[ "$must_join_files" == true ]]; then
-      play_in_vlc "$in_file"
+# Rename files based on user input
+name_audio_files() {
+  for file in $in_dir/*.WAV; do
+    if ! is_valid_file "$file"; then
+      continue
+    fi
+    new_file_name="$file"
+    has_file_moved=false
+    until is_valid_file_name "$new_file_name"; do
+      play_in_vlc "$file"
       printf "[yy-mm-dd_##]: "
       read new_file_name
-      if [ "$new_file_name" == "delete" ]; then
-        rm "$in_file"
-        has_file_moved=true
-        always_prompt=false
+      if [[ "$new_file_name" == "delete" ]]; then
+        rm "$file"
+        break
       elif is_valid_file_name "$new_file_name"; then
-        if [[ "$in_file" =~ 'join' ]]; then
+        if [[ "$file" =~ 'join' ]]; then
           wav_dir=$out_dir
         else
           wav_dir=$dir
         fi
-        mkdir -p "$wav_dir"
-        mv "$in_file" "$wav_dir/$new_file_nams.WAV"
-        has_file_moved=true
-        always_prompt=false
+        mkdir -p "$out_dir"
+        mv "$file" "$in_dir/$new_file_name.WAV"
+        break
       fi
-    else
-      i=$(( i + 1 ))
-    fi
+    done
   done
 }
 
-# Rename files based on user input
-name_audio_files() {
-  dir=$1
-  always_prompt=$2
-  for file in $dir/*.WAV; do
-    if ! is_valid_file "$file"; then
-      continue
+function ask_to_delete_join_dir {
+  [[ ! -d `join_dir` ]] && return 0
+  echo "JOIN CONTENTS"
+  echo "-------------"
+  ls `join_dir`
+  until [[ $should_delete_join_dir == "y" || $should_delete_join_dir == "n" ]]; do
+  echo "Do you want to delete the join directory? [y/n]: "
+  read should_delete_join_dir
+    if [[ $should_delete_join_dir = "y" ]]; then
+      rm -r `join_dir` 
+    else
+      return 0
     fi
-    get_filename_input "$file" "$dir"
   done
 }
 
 # Kickstart audio conversion process
 start_conversion() {
-  name_audio_files "$in_dir" false
-  convert_audio_files "$in_dir"
+  name_audio_files
+  convert_audio_files
 }
 
 start_conversion
