0421ca8df5f01c02a13eace05a1eb604fbfdef49
misigno <efournier92@gmail.com>
2020-02-05 21:56:27 -0500

    finish enhancing livestream and persist-stream bash scripts

--- Scripts/Bash/livestream/livestream.bash
+++ Scripts/Bash/livestream/livestream.bash
@@ -5,6 +5,7 @@ now_stamp=`date +'%Y-%m-%d-%H'`
 music_dir="/mnt/MD0/Audio/Music"
 livestream_dir="/home/misigno/scripts/livestream"
 playlist_file="$livestream_dir/Playlists/$now_stamp.txt"
+playlist_total_songs="200"
 input_device_main="/dev/video1"
 input_device_sub="/dev/video0"
 input_main_resolution="960:540"
@@ -23,32 +24,34 @@ stream_base_url="rtmp://${stream_server}.twitch.tv/app/"
 stream_repo="live_469920893_ZqDiWkgqq25jIsIgG23jEUjzY90wlI"
 stream_url="${stream_base_url}${stream_repo}"
 
+
 if [[ "$mode" = 'advanced' ]]; then
   rm -rf "$livestream_dir/Playlists"
   mkdir -p "$livestream_dir/Playlists"
   touch "$playlist_file"
 
-  find "$music_dir/." -name "*mp3" | sort -R | head -n 40 | while read file; do
+  find "$music_dir" -name "*mp3" | sort -R | head -n $playlist_total_songs | while read file; do
     if ! [[ "$file" == *"'"* ]]; then
       echo "file '$file'" >> "$playlist_file"
     fi
   done
-
+\
   ffmpeg -re \
-    -s "$input_main_resolution" -thread_queue_size "$thread_queue_max" -i "$input_device_main" \
-    -thread_queue_size "$thread_queue_max" -i "$input_device_sub" \
-    -f concat -safe 0 -i "$playlist_file" -c:a aac \
-    -fflags +igndts -fflags +genpts -copytb 1 \
-    -vcodec libx264 -g "$iframe_interval" -keyint_min "$iframe_minimum_interval" \
-    -b:v "$bitrate" -minrate "$bitrate" -maxrate "$bitrate" -pix_fmt yuv420p \
-    -preset "$quality_preset" \
-    -acodec libmp3lame -threads "$threads" -strict normal -tune zerolatency \
-    -filter_complex '[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[0:v]crop=700:540:260:0[cropped];[bg][cropped]overlay=0:(H-h)/2,crop=h=iw*9/16[withbg];[1:v]scale=iw/1.6:ih/1.6[pip];[withbg][pip]overlay=main_w-overlay_w-0:(H-h)/2[vid]' -map [vid] \
-    -bufsize "$bitrate" -f flv "$stream_url"
+  -s "$input_main_resolution" -thread_queue_size "$thread_queue_max" -i "$input_device_main" \
+  -thread_queue_size "$thread_queue_max" -i "$input_device_sub" \
+  -fflags +igndts -fflags +genpts -copytb 1 \
+  -f concat -safe 0 -i "$playlist_file" -c:a aac \
+  -vcodec libx264 -g "$iframe_interval" -keyint_min "$iframe_minimum_interval" \
+  -b:v "$bitrate" -minrate "$bitrate" -maxrate "$bitrate" -pix_fmt yuv420p \
+  -preset "$quality_preset" \
+  -acodec libmp3lame -threads "$threads" -strict normal -tune zerolatency \
+  -filter_complex '[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[0:v]crop=700:540:260:0[cropped];[bg][cropped]overlay=0:(H-h)/2,crop=h=iw*9/16[withbg];[1:v]scale=iw/1.6:ih/1.6[pip];[withbg][pip]overlay=main_w-overlay_w-0:(H-h)/2[vid]' -map [vid] -map 2:0 \
+  -bufsize "$bitrate" -f flv "$stream_url"
 
 else
   ffmpeg -re \
     -s "$input_main_resolution" -thread_queue_size "$thread_queue_max" -i "$input_device_main" \
+    -f concat -safe 0 -i "$playlist_file" -c:a aac \
     -fflags +igndts -fflags +genpts -copytb 1 \
     -vcodec libx264 -g "$iframe_interval" -keyint_min "$iframe_minimum_interval" \
     -b:v "$bitrate" -minrate "$bitrate" -maxrate "$bitrate" -pix_fmt yuv420p \
--- Scripts/Bash/livestream/persist-stream.bash
+++ Scripts/Bash/livestream/persist-stream.bash
@@ -1,36 +1,44 @@
 #!/bin/bash
 
-ffmpeg_id=32768
 this_process_start_time=0
 fails_in_10_minutes=0
+max_stream_hours="4"
+max_stream_seconds=$((max_stream_hours * 60 * 60))
+
+function kill_ffmpeg {
+  killall ffmpeg
+  sleep 1
+}
+
+kill_ffmpeg
+
 while true; do
   now_time=`date +%s`
-  seconds_since_last_stream=$((now_time-this_process_start_time))
-  echo "Stream Live for $seconds_since_last_stream Seconds"
+  seconds_since_last_stream=$((now_time - this_process_start_time))
 
-  if ! ps -p "$ffmpeg_id" > /dev/null; then
-    last_process_start_time="$this_process_start_time"
+  if ! pgrep "ffmpeg" || [ "$seconds_since_last_stream" -gt "$max_stream_seconds" ] ; then
+    kill_ffmpeg
+    sleep 1
     this_process_start_time=`date +%s`
-    seconds_since_last_stream=$((this_process_start_time-last_process_start_time))
 
-    if [[ "$fails_in_10_minutes" -lt 3 || "$seconds_since_last_stream" -gt 14400 ]]; then
-      kill  "$ffmpeg_id"
-      nohup bash /home/misigno/scripts/livestream/livestream.bash advanced &>/dev/null &
-      ffmpeg_id="$!"
+    if [[ "$fails_in_10_minutes" -lt 3 ]]; then
+      bash /home/misigno/scripts/livestream/livestream.bash advanced &
 
     else
-      kill "$ffmpeg_id"
-      nohup bash /home/misigno/scripts/livestream/livestream.bash &>/dev/null &
-      ffmpeg_id="$!"
+      bash /home/misigno/scripts/livestream/livestream.bash &
+
     fi
 
     if [[ "$seconds_since_last_stream" -lt 600 ]]; then
-      fails_in_10_minutes=$((fails_in_10_minutes+1))
+      fails_in_10_minutes=$((fails_in_10_minutes + 1))
 
     else
       fails_in_10_minutes=0
+
     fi
+
   fi
-  sleep 1 
+
+  sleep 60
 done
 
