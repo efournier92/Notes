507e2a0ab5a78904ed6d450e15affd1988a25825
misigno <efournier92@gmail.com>
2020-04-03 17:27:47 -0400

    enhance boot.bash

--- Scripts/Bash/boot.bash
+++ Scripts/Bash/boot.bash
@@ -12,17 +12,33 @@ sleep 1
 copyq &
 git config --global credential.helper "cache --timeout=1512000"
 
+function ssh_disable {
+  systemctl disable ssh;
+  systemctl stop ssh;
+}
+
+function ssh_enable {
+  systemctl enable ssh;
+  systemctl start ssh;
+}
+
+ssh_disable
+
 # Workhorse Laptop
 if [[ $motherboard_id = $huawei_2019_id || $motherboard_id = $macbook_2015_id ]]; then
   redshift -l 40.7:-73.9851 -t 5700:3800 -g 0.8 -m randr &
   m_bnk
+
 # Media Machine
 elif [[ $motherboard_id = $mhub_id ]]; then
-  kodi -fs &
+  ssh_enable
   m_MD
-  bash /home/misigno/scripts/Livestream/Livestream.bash
+  kodi -fs &
+  nohup pkexec /home/misigno/scripts/livestream/persist-stream.bash &>/home/misigno/scripts/livestream/livestream-output.txt
+
 # Other Machines
 else
   redshift -l 40.7:-73.9851 -t 5700:3800 -g 0.8 -m randr &
+  
 fi
 
