04d3852f2027f6c52b02705d9657b3d423f673a0
misigno <efournier92@gmail.com>
2020-04-04 12:20:05 -0400

    enhance backup-bnk.bash

--- Scripts/Bash/backup-bnk.bash
+++ Scripts/Bash/backup-bnk.bash
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-backup_drive=$1
-
+backup_drive="$1"
 date_stamp=`date +'%Y-%m-%d'`
-zip -r "/mnt/$backup_drive/SysBk/Sync/$date_stamp.zip" "/mnt/bnk/sync"
+zip_location="/mnt/$backup_drive/SysBk/Sync/$date_stamp.zip"
+sync_dir="/mnt/bnk/snc"
+trash_dir="$sync_dir/.trash"
+
+# Delete preexisiting zip, if one exists
+rm -rf "$zip_location"
+
+# Zip the sync directory
+zip -r --exclude=*Books* "$zip_location" "$sync_dir"
+
+# Clean trash folder
+rm -rf "$trash_dir"
 
