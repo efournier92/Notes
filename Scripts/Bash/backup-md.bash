01a26fe5c940afdfc7eb4ffc7892d68234d5f184
efournier92 <efournier92@gmail.com>
2020-06-15 17:58:18 -0400

    fix bugs in backup-md

--- Scripts/Bash/backup-md.bash
+++ Scripts/Bash/backup-md.bash
@@ -1,44 +1,36 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
+source_drive="/mnt/$1"
+md0_drive="/mnt/md0"
+md1_drive="/mnt/md1"
+audio_dir="$md0_drive/Audio"
+movies_dir="$md0_drive/Video/Movies"
+tv_dir="$md1_drive/Video/TV"
+
+if [[ -z "$1" ]]; then
   echo "Must Supply Source Disk"
   exit 1
 fi
 
-
-source_disk="/mnt/$1"
-md0_disk="/mnt/MD0"
-md1_disk="/mnt/MD1"
-audio_dir="$md0_disk/Audio"
-movies_dir="$md0_disk/Video/Movies"
-tv_dir="$md1_disk/Video/TV"
-
 should_backup() {
-  if ls -1qA "$source_disk" | grep -q . \
-    && ls -1qA "$md0_disk" | grep -q . \
-    && ls -1qA "$md1_disk" | grep -q .
-  then
-    true
-  else
-    false
-  fi
+  [[ $(ls -A "$source_drive") && $(ls -A "$md0_drive") && $(ls -A "$md1_drive") ]]
 }
 
 if should_backup; then
   mkdir -p "$audio_dir"
-  rclone sync "$source_disk/Media/Audio" "$audio_dir" --verbose
+  rclone sync "$source_drive/Media/Audio" "$audio_dir" --verbose
 
   mkdir -p "$movies_dir"
-  rclone sync "$source_disk/Media/Video/Movies" "$movies_dir" --verbose
+  rclone sync "$source_drive/Media/Video/Movies" "$movies_dir" --verbose
 
   mkdir -p "$tv_dir"
-  rclone sync "$source_disk/Media/Video/TV" "$tv_dir" --verbose
+  rclone sync "$source_drive/Media/Video/TV" "$tv_dir" --verbose
 
   echo " --------------------------"
   echo " DELETING EMPTY DIRECTORIES"
   echo " --------------------------"
-  pkexec find "$source_disk" -empty -type d -print -delete
-  pkexec find "$md0_disk" -empty -type d -print -delete
-  pkexec find "$md1_disk" -empty -type d -print -delete
+  find "$source_drive" -empty -type d -print -delete
+  find "$md0_drive" -empty -type d -print -delete
+  find "$md1_drive" -empty -type d -print -delete
 fi
 
