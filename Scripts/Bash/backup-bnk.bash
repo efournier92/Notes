2147efad37aaa9e1790e24a96593a639b2cf4786
efournier92 <efournier92@gmail.com>
2020-07-19 15:51:18 -0400

    add bk_log to backup_bnk script

--- Scripts/Bash/backup-bnk.bash
+++ Scripts/Bash/backup-bnk.bash
@@ -1,17 +1,33 @@
 #!/bin/bash
 
 backup_drive="$1"
+working_dir=`pwd`
 date_stamp=`date +'%Y-%m-%d'`
-zip_location="/mnt/$backup_drive/SysBk/Sync/$date_stamp.zip"
-sync_dir="/mnt/bnk/snc"
-trash_dir="$sync_dir/.trash"
+zip_location="/mnt/$backup_drive/Snc/$date_stamp.zip"
+sync_dir="/mnt/bnk"
+trash_dir="$sync_dir/snc/.trash"
+
+# Navigate to sync directory
+cd "$sync_dir"
 
 # Delete preexisiting zip, if one exists
 rm -rf "$zip_location"
 
 # Zip the sync directory
-zip -r "$zip_location" "$sync_dir"
+zip -r "$zip_location" "snc"
 
 # Clean trash directory
 rm -rf "$trash_dir"
+
+# Navigate back to original directory
+cd "$working_dir"
+
+# Load alias files
+shopt -s expand_aliases
+for file in ~/.zsh/aliases/*.zsh; do
+  source "$file"
+done
+
+# Backup log directory
+bk_lg
 
