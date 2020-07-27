8f38cc8756e1168f1eeb2a19012d78fc3bc26f11
efournier92 <efournier92@gmail.com>
2020-07-26 20:44:12 -0400

    enhance scan-to-pdf with additional user inputs

--- Scripts/Bash/scan-to-pdf.bash
+++ Scripts/Bash/scan-to-pdf.bash
@@ -1,40 +1,57 @@
 #!/bin/bash
 
-out_file_name="$1"
-scan_dir="scan-$out_file_name"
-out_concat_file="$scan_dir/out_concat"
-out_final_file="$out_file_name.pdf"
-out_seq_id="scan"
-out_seq_name="$out_seq_id%d.tif"
-scanner=`scanimage --formatted-device-list=%d`
+# scan-to-pdf
+# Outputs a multi-page PDF document from scanned input
 
-function stop_execution {
-  echo "EXITING"
-  exit
-}
+## Get document name as input
+while [[ -z "$scan_name" || -d "$scan_dir" ]]; do
+  read -p "Scan Name > " scan_name
+done
 
-if [[ -z "$scanner" ]]; then 
-  echo "NO SCANNER FOUND"
-  stop_execution
-fi
+## Calculate variables
+scan_dir="scan_${scan_name}"
+concat_file="${scan_dir}/out_concat"
+pdf_file="${scan_name}.pdf"
+sequence_id="scan"
+sequence_name="${sequence_id}%d.tif"
 
-if [[ -d "$scan_dir" ]]; then 
-  echo "DIRECTORY $scan_dir ALREADY EXISTS"
-  stop_execution
-fi
+## Get user selection for scan quality in dots-per-inch
+echo "-------------"
+echo "QUALITY (DPI)"
+echo "-------------"
+select quality in 150 300 600; do
+    echo "Selected $quality"
+    break
+done
 
-mkdir -p "$scan_dir"
+## Get user selection from available scanners
+echo "-------------"
+echo "SCANNER"
+echo "-------------"
+select scanner in `scanimage -f "%d %n"`; do
+    echo "Selected $scanner"
+    break
+done
+
+## Create temporary scan directory
+mkdir -p "${scan_dir}"
 
 ## Batch scan PDF scanner
-scanimage -d "$scanner" --batch="$scan_dir/$out_seq_name" --batch-prompt --format=tiff --resolution 300
+scanimage -d "${scanner}" --batch="${scan_dir}/${sequence_name}" --batch-prompt --progress --format=tiff --resolution 300
 
 ## Merge TIF and to PDF
-tiffcp $scan_dir/$out_seq_id*.tif "$out_concat_file.tif"
-tiff2pdf -o "$out_concat_file.pdf" "$out_concat_file.tif"
+tiffcp "${scan_dir}/${sequence_id}"*.tif "${concat_file}.tif"
+tiff2pdf -o "${concat_file}.pdf" "${concat_file}.tif"
 
 ## Shrink PDF
-ps2pdf "$out_concat_file.pdf" "$out_final_file"
+ps2pdf "${concat_file}.pdf" "${pdf_file}"
 
 ## Outut file location
-echo "OUTPUT PDF SAVED TO: $out_final_file"
+echo "OUTPUT PDF SAVED TO: ${pdf_file}"
+
+## Open Document
+evince "${pdf_file}"
+
+## Remove temporary scan directory
+rm -r "${scan_dir}"
 
